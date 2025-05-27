5cbd4b8411e53eeba015cf9d484b8ebd2a3b5a9f
Evgeny Shkvirya <45243533+Jengle88@users.noreply.github.com>
2025-05-27 20:21:30 +0300

    #95: Added logcat error message (#96)

--- plugin/MoevmChecker/src/main/kotlin/ru/moevm/moevm_checker/core/tasks/codetask/platforms/android/AndroidTaskCodeCheckSystem.kt
+++ plugin/MoevmChecker/src/main/kotlin/ru/moevm/moevm_checker/core/tasks/codetask/platforms/android/AndroidTaskCodeCheckSystem.kt
@@ -1,11 +1,15 @@
 package ru.moevm.moevm_checker.core.tasks.codetask.platforms.android
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import ru.moevm.moevm_checker.core.tasks.TaskConstants
 import ru.moevm.moevm_checker.core.tasks.codetask.AbstractCodeCheckSystem
 import ru.moevm.moevm_checker.core.tasks.codetask.CheckResult
 import ru.moevm.moevm_checker.core.tasks.codetask.CodeTaskResult
 import ru.moevm.moevm_checker.core.tasks.codetask.platforms.gradle.GradleCommandLine
+import ru.moevm.moevm_checker.core.utils.coroutine.catchWithLog
 import java.io.File
 
 class AndroidTaskCodeCheckSystem(
@@ -29,12 +33,26 @@ class AndroidTaskCodeCheckSystem(
         }
         val checkResults = mutableListOf<TaskResult>()
         val logcatCollector = AndroidLogcatCollector()
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
+        var cancelledResult: CodeTaskResult? = null
+        var logcatCheckerState = ""
         if (taskArgs.isNotEmpty()) {
-            logcatCollector.start(selector = { line ->
-                line.contains(TaskConstants.CHECKER_FLAG)
-            }, Dispatchers.IO)
+            logcatCollector
+                .start(selector = { line ->
+                    line.contains(TaskConstants.CHECKER_FLAG)
+                })
+                .catchWithLog { e ->
+                    cancelledResult = CodeTaskResult(CheckResult.Error(getPrettyExceptionMessage(e)), "", "")
+                }
+                .onEach { str ->
+                    logcatCheckerState = str
+                }
+                .launchIn(coroutineScope)
         }
         for (arg in taskArgs) {
+            if (cancelledResult != null) {
+                break
+            }
             when (arg) {
                 "unit_test" -> {
                     val result = runUnitTests(jdkPath, taskFolder)
@@ -43,6 +61,7 @@ class AndroidTaskCodeCheckSystem(
                     }
                     checkResults.add(TaskResult("Unit tests", result))
                 }
+
                 "instrumental_test" -> {
                     val result = runInstrumentalTests(jdkPath, taskFolder)
                     if (result.result is CheckResult.Failed || result.result is CheckResult.Error) {
@@ -52,21 +71,32 @@ class AndroidTaskCodeCheckSystem(
                 }
             }
         }
-        val probablyResultCode = logcatCollector.collect().lastOrNull()?.takeLastWhile { it != ' ' }
-        return if (checkResults.all { it.taskResult.result is CheckResult.Passed }) {
-            CodeTaskResult(
-                CheckResult.Passed,
-                checkResults.joinToString( "\n\n\n") { "${it.arg}:\n\n" + it.taskResult.stdout },
-                checkResults.joinToString("\n\n\n") {
-                    if (it.taskResult.stderr.isBlank()) {
-                        ""
-                    } else {
-                        "${it.arg}:\n\n" + it.taskResult.stderr }
-                }.ifBlank { "" },
-                probablyResultCode
-            )
+        val probablyResultCode = logcatCheckerState.takeLastWhile { it != ' ' }
+        return cancelledResult
+            ?: if (checkResults.all { it.taskResult.result is CheckResult.Passed }) {
+                CodeTaskResult(
+                    CheckResult.Passed,
+                    checkResults.joinToString("\n\n\n") { "${it.arg}:\n\n" + it.taskResult.stdout },
+                    checkResults.joinToString("\n\n\n") {
+                        if (it.taskResult.stderr.isBlank()) {
+                            ""
+                        } else {
+                            "${it.arg}:\n\n" + it.taskResult.stderr
+                        }
+                    }.ifBlank { "" },
+                    probablyResultCode
+                )
+            } else {
+                CodeTaskResult(CheckResult.Failed, "", "Unknown error")
+            }
+    }
+
+    private fun getPrettyExceptionMessage(e: Throwable?): String {
+        val startMessage = "Error during test: "
+        return if (e?.message?.contains("Cannot run program \"adb\"") == true) {
+            startMessage + (e.message + ". Check \"adb\" path in you PATH or download it.")
         } else {
-            CodeTaskResult(CheckResult.Failed, "", "Unknown error")
+            "$startMessage ${e?.message}"
         }
     }
 
